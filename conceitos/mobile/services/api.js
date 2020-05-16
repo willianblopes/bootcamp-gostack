a4827ca5339091fdccc38150fde720d473ccddb8
Willian Barreto <willianblopes@gmail.com>
2020-05-15 22:17:04 -0300

    conceitos react native

---
+++ conceitos/mobile/services/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3333',
+});
+
+export default api;
+
+/**
+ * iOS com Emulador: localhost
+ * iOS fisico: IP da maquina
+ * Android com Emulador: localhost (ADB REVERSE)
+ * Android com Emulador: 10.0.2.2 (Android Studio)
+ * Android com Emulador: 10.0.3.2 (Genymotion)
+ * Android com fisico: IP da maquina
+ */
